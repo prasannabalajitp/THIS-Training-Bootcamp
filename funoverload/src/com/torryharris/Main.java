394b43adbd03a31ff9cf823812ef23606e7acf54
Shivakumar.R.Hombal <58224595+Shivakumar-coder@users.noreply.github.com>
2021-10-01 09:33:18 +0530

    Add files via upload

---
+++ funoverload/src/com/torryharris/Main.java
@@ -0,0 +1,19 @@
+package com.torryharris;
+
+import com.torryharris.utility.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*summation su=new summation();
+        System.out.println(su.add(10,30.5f));*/
+
+        average avg=new average();
+        System.out.println(avg.calc_avg(10,15.5f));
+
+
+        /*Tax t=new Tax();
+        System.out.println(t.calc_tax(20000,'c'));
+        System.out.println(t.calc_tax(20000));*/
+    }
+}
